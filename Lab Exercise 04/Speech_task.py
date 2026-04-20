73960b8dff243b4a5318fa8108663985965cd3ea
Gosankyo <anxofangar@gmail.com>
2026-04-20 11:46:47 +0200

    libraries

---
+++ Lab Exercise 04/Speech_task.py
@@ -0,0 +1,10 @@
+# Libraries
+from __future__ import annotations
+
+from dataclasses import dataclass
+from pathlib import Path
+from typing import Dict, Optional, Tuple
+
+import numpy as np
+import wave
+import cv2 
